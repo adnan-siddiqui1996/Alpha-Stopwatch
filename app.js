37a3dedfbd94010751baec961028b460d2aaa2e5
Adnan-siddiqui1996 <adnanahmed.siddiqui1996@gmail.com>
2020-07-14 04:10:40 +0500

    Time lapse

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ var secHeading = document.getElementById('sec');
 var msecHeading = document.getElementById('msec');
 var interval;
 var sound = new Audio('Ticking.mp3');
-document.getElementById('pause').disabled = true;
-document.getElementById('soundPlay').disabled = true;
-document.getElementById('soundPause').disabled = false;
+document.getElementById('soundPause').style.visibility = "visible";
+document.getElementById('timeLapse').disabled = true;
 var a = true;
+var count = 0;
+
+var nHrs;
+var nMins; 
+var nSec;
+var nMsec;
 
 function timer() {
     msec++;
@@ -56,39 +61,50 @@ function timer() {
 }
 
 function soundPlay() {
-    document.getElementById('soundPlay').disabled = true;
-    document.getElementById('soundPause').disabled = false;
+    document.getElementById('soundPlay').style.visibility = "hidden";
+    document.getElementById('soundPause').style.visibility = "visible";
     a = true;
     sound.play();
 }
 
 function soundPause() {
-    document.getElementById('soundPlay').disabled = false;
-    document.getElementById('soundPause').disabled = true;
+    document.getElementById('soundPlay').style.visibility = "visible";
+    document.getElementById('soundPause').style.visibility = "hidden";
     a = false;
     sound.pause();
 }
 
 function play() {
+    document.getElementById('pause').style.visibility = "visible";
+    document.getElementById('play').style.visibility = "hidden";
+    document.getElementById('timeLapse').disabled = false;
     var animation = document.getElementById('outside');
     animation.style.animation = "spin 1s linear infinite";
-    document.getElementById('play').disabled = true;
     document.getElementById('pause').disabled = false;
     interval = setInterval(timer, 10)
 }
 function pause() {
+    document.getElementById('pause').style.visibility = "hidden";
+    document.getElementById('play').style.visibility = "visible";
     clearInterval(interval);
     var animation = document.getElementById('outside');
-    animation.style.animation = "";
-    document.getElementById('play').disabled = false;
-    document.getElementById('pause').disabled = true;
+    animation.style.animation = ""; 
 }
-function reset() {
+function reset() {    
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    count = 0;
+    document.getElementById('timeLapse').disabled = true;
+    var div = document.getElementById('timeRecord');
+    div.innerHTML = "";    
+    div.style.overflowY = "hidden";
+    document.getElementById('pause').style.visibility = "hidden";
+    document.getElementById('play').style.visibility = "visible";
+    document.getElementById('record').style.visibility = "hidden";
+    document.getElementById('body1').style.overflowY = "hidden";
     clearInterval(interval);
     var animation = document.getElementById('outside');
     animation.style.animation = "";
-    document.getElementById('play').disabled = false;
-    document.getElementById('pause').disabled = true;
     hrs = 00;
     mins = 00;
     sec = "00";
@@ -98,3 +114,35 @@ function reset() {
     secHeading.innerHTML = sec;
     msecHeading.innerHTML = msec;    
 }
+function timeLapse() {
+    document.getElementById('record').style.visibility = "visible";
+    document.getElementById('body1').style.overflowY = "scroll";
+    var div = document.getElementById('timeRecord');
+    count++;
+    if(count > 6) {
+        div.style.overflowY = "Scroll";
+    }
+    if(count > 1) {
+        nHrs = hrs - nHrs; 
+        nMins = mins - nMins; 
+        nSec = sec - nSec;
+        nMsec = msec - nMsec;
+    }
+    else{
+        nHrs = hrs; 
+        nMins = mins; 
+        nSec = sec;
+        nMsec = msec;
+    }
+    var newItem = document.createElement("p");
+    var textnode = document.createTextNode("#" + count + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + Math.abs(nHrs) + ":" + Math.abs(nMins) + ":" + Math.abs(nSec) + ":" + Math.abs(nMsec) + "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + hrs + ":" + mins + ":" + sec + ":" + msec);
+    newItem.appendChild(textnode);
+    div.insertBefore(newItem, div.childNodes[0]);
+    nHrs = hrs; 
+    nMins = mins; 
+    nSec = sec;
+    nMsec = msec;
+}
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
